4d80a72187da6d7dcf58f47912b9b96a75c18a4d
Ben Hansen <bhbenjaminhansen@gmail.com>
2022-01-19 15:37:12 -0700

    test 07 is working

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,18 +1,70 @@
+use anyhow::bail;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use syn::{
-    parse_macro_input, AngleBracketedGenericArguments, Data, DataStruct, DeriveInput,
-    GenericArgument, Ident, Path, PathArguments, Type, TypePath,
+    parse_macro_input, AngleBracketedGenericArguments, Attribute, Data, DataStruct,
+    DeriveInput, Field, GenericArgument, Meta, MetaList, MetaNameValue,
+    NestedMeta, Path, PathArguments, Type, TypePath, Lit, Ident,
 };
 
 enum FieldWrapperType<'a> {
     None,
     Option(&'a Type),
-    Vec(&'a Type),
+    Vec {
+        inner_ty: &'a Type,
+        each: Option<String>,
+    },
 }
 
-#[proc_macro_derive(Builder)]
+fn compare_path_with_str(p: &Path, s: &str) -> bool {
+    let parts = p
+        .segments
+        .iter()
+        .map(|seg| seg.ident.to_string())
+        .collect::<Vec<_>>();
+    parts.join("::") == s
+}
+
+fn find_builder_attrs(f: &Field) -> Vec<&Attribute> {
+    f.attrs
+        .iter()
+        .filter(|a| {
+            let segments = &a.path.segments;
+            match segments.len() {
+                1 => {
+                    let ai = &segments[0].ident;
+                    &ai.to_string() == "builder"
+                }
+                _ => false,
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn each_from_attribute(a: &Attribute) -> Result<String, anyhow::Error> {
+    let meta = a.parse_meta()?;
+
+    let nested = match meta {
+        Meta::List(MetaList { path, nested, .. })
+            if compare_path_with_str(&path, "builder") && nested.len() == 1 =>
+        {
+            nested
+        }
+        _ => bail!("Only builder(each = \"...\") is supported"),
+    };
+
+    match &nested[0] {
+        NestedMeta::Meta(Meta::NameValue(MetaNameValue {
+            path: nested_path,
+            lit: Lit::Str(lit),
+            ..
+        })) if compare_path_with_str(&nested_path, "each") => Ok(lit.value()),
+        _ => bail!("Only builder(each = \"...\") is supported"),
+    }
+}
+
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -25,6 +77,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
     .into_iter()
     .map(|f| {
+        // Process attributes
+        let attrs = find_builder_attrs(f);
+        let mut each = None;
+        for a in attrs {
+            each = Some(each_from_attribute(a).unwrap());
+        }
+
+        // Process wrapper types
         let segments = match f.ty {
             Type::Path(TypePath {
                 qself: None,
@@ -32,7 +92,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }) => segments,
             _ => unimplemented!(),
         };
-
         let seg = &segments[0];
         let segi = &seg.ident;
         let ty_name = segi.to_string();
@@ -53,12 +112,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 if &ty_name[..] == "Option" {
                     FieldWrapperType::Option(inner_ty)
                 } else {
-                    FieldWrapperType::Vec(inner_ty)
+                    FieldWrapperType::Vec { inner_ty, each }
                 }
             }
             _ => FieldWrapperType::None,
         };
-        let opt = segi == &Ident::new("Option", segi.span());
 
         (f, wrapper_ty)
     })
@@ -81,8 +139,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
         .iter()
         .map(|(f, wrapper_ty)| {
             let fi = f.ident.clone().unwrap();
-            quote! {
-                #fi: None
+            match wrapper_ty {
+                FieldWrapperType::Vec { each: Some(_), .. } => quote! {
+                    #fi: Some(vec![])
+                },
+                _ => quote! {
+                    #fi: None
+                },
             }
         })
         .collect::<Vec<_>>();
@@ -92,7 +155,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
         .map(|(f, wrapper_ty)| {
             let fi = f.ident.clone().unwrap();
             match wrapper_ty {
-                FieldWrapperType::Option(_) => quote! { #fi: self.#fi.clone() },
+                FieldWrapperType::Option(_) => {
+                    quote! { #fi: self.#fi.clone() }
+                }
                 _ => quote! { #fi: self.#fi.clone().ok_or("No value for field")? },
             }
         })
@@ -100,6 +165,18 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let methods = st_fields
         .iter()
+        .filter(|(f, wrapper_ty)| {
+            if let Some(f_name) = &f.ident {
+                match wrapper_ty {
+                    FieldWrapperType::Vec {
+                        each: Some(each), ..
+                    } if each == &f_name.to_string() => false,
+                    _ => true,
+                }
+            } else {
+                false
+            }
+        })
         .map(|(f, wrapper_ty)| {
             let fi = f.ident.clone().unwrap();
             let fty = &f.ty;
@@ -108,12 +185,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 FieldWrapperType::Option(ty) => ty,
                 _ => fty,
             };
-
             quote! {
                 pub fn #fi(&mut self, val: #inner_ty) -> &mut Self {
                     self.#fi = std::option::Option::Some(val);
                     self
                 }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let each_methods = st_fields
+        .iter()
+        .filter(|(_f, wrapper_ty)| match wrapper_ty {
+            FieldWrapperType::Vec { each: Some(_), .. } => true,
+            _ => false,
+        })
+        .map(|(f, wrapper_ty)| {
+            let fi = f.ident.clone().unwrap();
+            match wrapper_ty {
+                FieldWrapperType::Vec {
+                    inner_ty,
+                    each: Some(each),
+                } => {
+                    let each = Ident::new(each, fi.span());
+                    quote! {
+                        pub fn #each(&mut self, val: #inner_ty) -> &mut Self {
+                            if let ::std::option::Option::Some(v) = &mut self.#fi {
+                                v.push(val);
+                            } else {
+                                self.#fi = ::std::option::Option::Some(vec![val]);
+                            }
+                            self
+                        }
+                    }
+                }
+                _ => unreachable!(),
             }
         })
         .collect::<Vec<_>>();
@@ -133,6 +239,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_ident {
             #(#methods)*
+            #(#each_methods)*
 
             pub fn build(&self) -> Result<#st_ident, Box<dyn std::error::Error>> {
                 Ok(#st_ident {
